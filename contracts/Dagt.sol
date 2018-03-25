362e62f757a5fccf51834298d84720abac62b26a
dingzhongchang <zhongchang.ding@gmail.com>
2018-03-25 09:50:17 +0800

    Update Dagt.sol
    extends PausableToken

--- contracts/Dagt.sol
+++ contracts/Dagt.sol
@@ -1,37 +1,62 @@
 pragma solidity ^0.4.17;
 
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "zeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
 
-contract Dagt is MintableToken {
-    string public constant version = "1.0";
+/**
+ * @dev DAGT token ERC20 contract
+ * Based on references from OpenZeppelin: https://github.com/OpenZeppelin/zeppelin-solidity
+ */
+contract Dagt is MintableToken, PausableToken {
+    string public constant version = "1.1";
     string public constant name = "DAGT Crypto Platform";
     string public constant symbol = "DAGT";
     uint8 public constant decimals = 18;
 
-    modifier onlyMintingFinished() {
-        require(mintingFinished == true);
+    event MintMasterTransferred(address indexed previousMaster, address indexed newMaster);
+    
+    address public mintMaster;
+
+    modifier onlyMintMasterOrOwner() {
+        require(msg.sender == mintMaster || msg.sender == owner);
         _;
     }
-    /// @dev Same ERC20 behavior, but require the token to be unlocked
-    /// @param _spender address The address which will spend the funds.
-    /// @param _value uint256 The amount of tokens to be spent.
-    function approve(address _spender, uint256 _value) public onlyMintingFinished returns (bool) {
-        return super.approve(_spender, _value);
+
+    function Dagt() public {
+        mintMaster = msg.sender;
+    }
+        
+    function transferMintMaster(address newMaster) onlyOwner public {
+        require(newMaster != address(0));
+        MintMasterTransferred(mintMaster, newMaster);
+        mintMaster = newMaster;
     }
 
-    /// @dev Same ERC20 behavior, but require the token to be unlocked
-    /// @param _to address The address to transfer to.
-    /// @param _value uint256 The amount to be transferred.
-    function transfer(address _to, uint256 _value) public onlyMintingFinished returns (bool) {
-        return super.transfer(_to, _value);
+    function mintToAddresses(address[] addresses, uint256 amount) public onlyMintMasterOrOwner canMint {
+        for (uint i = 0; i < addresses.length; i++) {
+            require(mint(addresses[i], amount));
+        }
     }
 
-    /// @dev Same ERC20 behavior, but require the token to be unlocked
-    /// @param _from address The address which you want to send tokens from.
-    /// @param _to address The address which you want to transfer to.
-    /// @param _value uint256 the amount of tokens to be transferred.
-    function transferFrom(address _from, address _to, uint256 _value) public onlyMintingFinished returns (bool) {
-        return super.transferFrom(_from, _to, _value);
+    function mintToAddressesAndAmounts(address[] addresses, uint256[] amounts) public onlyMintMasterOrOwner canMint {
+        require(addresses.length == amounts.length);
+        for (uint i = 0; i < addresses.length; i++) {
+            require(mint(addresses[i], amounts[i]));
+        }
     }
+    /**
+     * @dev Function to mint tokens
+     * @param _to The address that will receive the minted tokens.
+     * @param _amount The amount of tokens to mint.
+     * @return A boolean that indicates if the operation was successful.
+     */
+    function mint(address _to, uint256 _amount) onlyMintMasterOrOwner canMint public returns (bool) {
+        address oldOwner = owner;
+        owner = msg.sender;
+        bool result = super.mint(_to, _amount);
+        owner = oldOwner;
+        return result;
+    }
+
 
 }
